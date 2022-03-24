c3ae61302577d44f2296ed44e52cbbc1a265ebf5
ijann <sugey90210@gmail.com>
2022-03-24 14:18:24 -0300

    usando linter - revive

--- main.go
+++ main.go
@@ -3,8 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go")
+	PrintHello()
 	for i:=0; i < 5; i++ {
-		fmt.Println(i)
+		PrintNumber(i)
 	}
+}
+
+//revive:disable:exported
+
+// PrintHello escribe el clásico Hola 
+func PrintHello() {
+	fmt.Println("Hello, Go")
+}
+
+//revive:disable:exported
+
+// PrintNumber escribe un número usando la función fmt.Println
+func PrintNumber(number int) {
+	fmt.Println(number)
 }
